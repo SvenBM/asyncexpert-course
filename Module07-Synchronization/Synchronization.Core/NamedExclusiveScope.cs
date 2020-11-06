8b900df296d1afc51a799e8325e0b0fd4eb8c3bb
Sven Müller <sven.mueller@haevg-rz.de>
2020-11-06 07:52:28 +0100

    mutex + semaphore

--- Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
+++ Module07-Synchronization/Synchronization.Core/NamedExclusiveScope.cs
@@ -11,8 +11,33 @@ namespace Synchronization.Core
      */
     public class NamedExclusiveScope : IDisposable
     {
+        private readonly Mutex mutex;
+        private readonly Semaphore semaphore;
+
         public NamedExclusiveScope(string name, bool isSystemWide)
         {
+            if(isSystemWide)
+            {
+                if (Mutex.TryOpenExisting(name, out this.mutex))
+                    throw new InvalidOperationException($"Unable to get a global lock {name}.");
+
+                this.mutex = new Mutex(false, name);
+                this.mutex.WaitOne();
+            }
+            else
+            {
+                if(!Semaphore.TryOpenExisting(name, out this.semaphore))
+                    this.semaphore = new Semaphore(1,1, name);
+                this.semaphore.WaitOne();
+            }
+        }
+
+        public void Dispose()
+        {
+            this.mutex?.ReleaseMutex();
+            this.mutex?.Dispose();
+            this.semaphore?.Release();
+            this.semaphore?.Dispose();
         }
     }
 }
