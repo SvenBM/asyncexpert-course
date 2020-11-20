618dce60f5a0bbf0b0080366bd2e3dfe11d76c03
Sven Müller <sven.b.mueller@googlemail.com>
2020-11-20 14:07:55 +0100

    benchmark async

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool b)
+        {
+            return new BoolAwaiter(b);
+        }
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
+        private readonly bool b;
+
+        public bool IsCompleted => false;
+
+        public BoolAwaiter(bool b)
+        {
+            this.b = b;
+        }
+
         public void OnCompleted(Action continuation)
         {
-            throw new NotImplementedException();
+            _ = Task.Run(() => continuation?.Invoke());
         }
+
+        public bool GetResult() => b;
     }
 }
--- Module08-Concurrent Data Structures/DataStructures.Benchmark/MetricsCounterBenchmark.cs
+++ Module08-Concurrent Data Structures/DataStructures.Benchmark/MetricsCounterBenchmark.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
 namespace DataStructures.Benchmark
 {
-    public class LockingMetricsCounter
+    [MemoryDiagnoser]
+    public class MetricsCounterBenchmark
     {
-        public LockingMetricsCounter()
+        private const int KeyCount = 16;
+        private const int ValueCount = 100000;
+        private const int ConcurrentWriters = 2;
+
+        [Params(typeof(LockingMetricsCounter), typeof(ConcurrentDictionaryOnlyMetricsCounter), typeof(ConcurrentDictionaryWithCounterMetricsCounter))]
+        public Type Type;
+
+        private Task[] tasks;
+        private TaskCompletionSource<object> starter;
+
+        [IterationSetup]
+        public void Setup()
         {
+            var originalKeys = Enumerable.Range(0, KeyCount).Select(i => i.ToString()).ToArray();
+            var keys = Enumerable.Repeat(originalKeys, ConcurrentWriters).SelectMany(m => m).ToArray();
+
+            starter = new TaskCompletionSource<object>();
+            var counter = (IMetricsCounter)Activator.CreateInstance(Type);
+
+            // run two tasks per key
+            tasks = keys.Select(key => Task.Run(async () =>
+            {
+                await starter.Task;
+                for (var i = 0; i < ValueCount; i++)
+                {
+                    counter.Increment(key);
+                }
+            })).ToArray();
+        }
+
+        [Benchmark]
+        public async Task Run()
+        {
+            starter.SetResult(starter);
+            await Task.WhenAll(tasks);
         }
     }
 }
